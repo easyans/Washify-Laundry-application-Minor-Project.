1a58f3d298b36f24165764e117cd6f53baf133e8
Aakash kumar raj <ar0829@srmist.edu.in>
2024-02-14 11:16:10 +0530

    CSE Minor Project

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { Provider } from 'react-redux';
+import store from "./Store";
+import StackNavigator from './StackNavigator';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <StackNavigator />
+      <StatusBar style="auto" />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLXKSOXTCK5R-C4_cJEALs6M4wi40G1es",
+  authDomain: "laundry-application-843ea.firebaseapp.com",
+  projectId: "laundry-application-843ea",
+  storageBucket: "laundry-application-843ea.appspot.com",
+  messagingSenderId: "758125621047",
+  appId: "1:758125621047:web:7ff25d4272153fdcc774b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+const db = getFirestore();
+
+export {auth,db};
